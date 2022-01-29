50c0ef60a704b1c3e10569800443da67f365b5a2
PARMESHWARPANWAR <parmeshwarpanwar777@gmail.com>
2022-01-29 12:05:18 +0530

    [netflix] add firebase folder and firebase.js and data

---
+++ netflix-clone/src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmdZvzLITZo4DZtDhE2-kPzH6Vb6LmZjg",
+    authDomain: "netflix-clone-4a7f6.firebaseapp.com",
+    projectId: "netflix-clone-4a7f6",
+    storageBucket: "netflix-clone-4a7f6.appspot.com",
+    messagingSenderId: "320907768090",
+    appId: "1:320907768090:web:e5ce342f88350b1dc9bca3"
+  };
