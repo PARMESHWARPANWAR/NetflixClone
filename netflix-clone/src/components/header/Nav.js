841f93f8832da1a7e3a92769c7a1702e0de07552
PARMESHWARPANWAR <parmeshwarpanwar777@gmail.com>
2022-01-29 13:05:00 +0530

    add homeScreen Nav bar and Navbar transition animation

---
+++ netflix-clone/src/components/header/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://www.pxpng.com/public/uploads/preview/-11621687998ytrgnduxoh.png"
+          alt="Logo"
+        />
+
+        <img className="nav_avatar" src="" alt="twimg.com/profileImage" />
+        <h1>This is nav</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
