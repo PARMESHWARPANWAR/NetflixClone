4b941fe9f0b8bae86716afe49e4ff3de575390b2
PARMESHWARPANWAR <parmeshwarpanwar777@gmail.com>
2022-01-30 11:55:33 +0530

    [netflix] fetch data from api and show on banner

--- netflix-clone/src/components/header/Banner.js
+++ netflix-clone/src/components/header/Banner.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import axios from "./axios";
+import request from "./request";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(request.fetchNetflixOriginals);
 
-  function truncate(string,n){
-    return string?.length>n ? string.substr(0,n-1)+'...': string;
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
-  return <header className="banner"
-    style={{backgroundSize:"cover",
-    backgroundImage:`url("https://wallpapercave.com/wp/wp7123665.jpg")`,
-    backgroundPosition:"center center",
-}}  
-  >
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
       <div className="banner_contents">
-          <h1 className="banner_title">Movie Name</h1>
-          <div className="banner_buttons">
-              <button className="banner_button">Play</button>
-              <button className="banner_button">My List</button>
-          </div>
-          <h1 className="banner_description" style={{ color: 'white' }}>
-            {truncate(`This is a test description
-          This is a test description This is a test description This is a test description
-          This is a test description This is a test description This is a test description
-          This is a test description This is a test description This is a test description
-          This is a test description This is a test description This is a test description
-          This is a test description This is a test description This is a test description
-          This is a test description This is a test description This is a test description`,150)}</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name} 
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description" style={{ color: "white" }}>
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
-      <div className="banner_fadeBotton"/>
-  </header>;
+      <div className="banner_fadeBotton" />
+    </header>
+  );
 }
 
 export default Banner;
