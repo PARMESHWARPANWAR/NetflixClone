4a9f5551905633d3acbac4435c87c5bd445487c7
PARMESHWARPANWAR <parmeshwarpanwar777@gmail.com>
2022-01-30 14:29:20 +0530

    row part completed

--- netflix-clone/src/components/header/Banner.js
+++ netflix-clone/src/components/header/Banner.js
@@ -20,6 +20,8 @@ function Banner() {
     fetchData();
   }, []);
 
+  // console.log(movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
